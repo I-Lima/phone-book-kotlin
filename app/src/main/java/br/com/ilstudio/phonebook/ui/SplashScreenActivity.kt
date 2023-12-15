41b67999ee74c49482f187c97cf216e2f222862a
Ingrid Lima <ibezerradelima@gmail.com>
2023-12-15 16:19:36 -0300

    :sparkles: feat: Add Splash Screen

--- app/src/main/java/br/com/ilstudio/phonebook/ui/SplashScreenActivity.kt
+++ app/src/main/java/br/com/ilstudio/phonebook/ui/SplashScreenActivity.kt
@@ -1,10 +1,11 @@
-package br.com.ilstudio.phonebook
+package br.com.ilstudio.phonebook.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import br.com.ilstudio.phonebook.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
