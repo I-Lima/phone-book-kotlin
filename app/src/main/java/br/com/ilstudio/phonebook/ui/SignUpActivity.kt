df0536afb276fe938c77546cec33c57493d281d3
Ingrid Lima <ibezerradelima@gmail.com>
2023-12-18 14:28:39 -0300

    :sparkles: feat: Add user image

--- app/src/main/java/br/com/ilstudio/phonebook/ui/SignUpActivity.kt
+++ app/src/main/java/br/com/ilstudio/phonebook/ui/SignUpActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.com.ilstudio.phonebook.R
 import br.com.ilstudio.phonebook.database.DBHelper
 import br.com.ilstudio.phonebook.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private var id: Int? = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -19,10 +24,12 @@ class SignUpActivity : AppCompatActivity() {
 
         binding.buttonSignUp.setOnClickListener {
             val username = binding.editUsername.text.toString()
-            val passoword = binding.editPassword.text.toString()
-            val confirmPassord = binding.editConfirmPassword.text.toString()
+            val password = binding.editPassword.text.toString()
+            val confirmPassword = binding.editConfirmPassword.text.toString()
+            var userImageId = -1
+            if(id != null) userImageId = id as Int
 
-            if(username.isEmpty() || passoword.isEmpty() || confirmPassord.isEmpty()) {
+            if(username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
                 Toast.makeText(
                     this,
                     getString(R.string.please_insert_all_required_fields),
@@ -30,8 +37,8 @@ class SignUpActivity : AppCompatActivity() {
                 ).show()
             }
 
-            if(passoword == confirmPassord) {
-                val response = db.insertUser(username, passoword)
+            if(password == confirmPassword) {
+                val response = db.insertUser(username = username, password = password, imageId = userImageId)
 
                 if(response > 0) {
                     Toast.makeText(
@@ -63,6 +70,23 @@ class SignUpActivity : AppCompatActivity() {
         binding.buttonAccont.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        }
+
+        binding.imageContact.setOnClickListener {
+            launcher.launch(Intent(applicationContext, ContactImageSelectionActivity::class.java))
+        }
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.data != null && it.resultCode == 1) {
+                id = it.data?.extras?.getInt("id")
+
+                binding.imageContact.setImageResource(id!!)
+            } else if(it.resultCode == 2) {
+                println()
+            } else {
+                id = -1
+                binding.imageContact.setImageResource(R.drawable.ic_profile_default)
+            }
         }
     }
 }
